3385525df5231c37b98aaaaf831b80d16335e62e
Josh Walters <josh.walters@outlook.com>
2020-10-30 15:18:45 +0000

    updated to create vm on store

--- src/vue-devtool-plugin.ts
+++ src/vue-devtool-plugin.ts
@@ -1,21 +1,59 @@
+import Vue from 'vue';
 import { IStateliStore } from 'stateli';
 
 const target: any = typeof window !== 'undefined' ? window : typeof (global as any) !== 'undefined' ? global : {};
 const devtoolHook = target.__VUE_DEVTOOLS_GLOBAL_HOOK__;
 
+const initVm = (store: IStateliStore<any>) => {
+  const storeAny: any = store;
+  if (!storeAny._devtoolHook) {
+    storeAny._devtoolHook = devtoolHook;
+  }
+  if (!storeAny.replaceState) {
+    storeAny.replaceState = x => {
+      storeAny.state = x;
+      storeAny._vm.$$state = x;
+    };
+  }
+
+  const oldVm = storeAny._vm;
+
+  const computed: any = {};
+  for (const m of store.modules) {
+    for (const g of m.getters) {
+      const key = m.namespaced ? `${m.name}/${g.type}` : g.type;
+      Object.defineProperty(computed, key, {
+        get: () => {
+          return g.getValue(m.state, store.getter, store.state);
+        },
+        enumerable: true,
+      });
+    }
+  }
+  const silent = Vue.config.silent;
+  Vue.config.silent = true;
+  storeAny._vm = new Vue({
+    data: {
+      $$state: store.state
+    },
+    computed,
+  });
+  Vue.config.silent = silent;
+
+  if (!!oldVm) {
+    Vue.nextTick(() => oldVm.$destroy());
+  }
+};
+
 export default (store: IStateliStore<any>) => {
   if (!devtoolHook) return;
 
-  const storeAny: any = store;
-  storeAny._devtoolHook = devtoolHook;
-  if (!storeAny.replaceState) {
-    storeAny.replaceState = x => (storeAny.state = x);
-  }
+  initVm(store);
 
-  devtoolHook.emit('vuex:init', storeAny);
+  devtoolHook.emit('vuex:init', store);
 
   devtoolHook.on('vuex:travel-to-state', targetState => {
-    storeAny.replaceState(targetState);
+    (store as any).replaceState(targetState);
   });
 
   store.subscribeToMutation(s => {
